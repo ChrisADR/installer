34b12385f2b844b009b026e70dcf1f034beee364
Christopher Díaz Riveros <chrisadr@gentoo.org>
2017-12-19 14:46:53 -0500

    pym/stager/main.py: argparse implementation
    Argparse implementation considers two actions and four options:
    
    ACTIONS:
     -begin : Begin a new Gentoo Installation Process
     -generate : Generate a stageX tarball with particular files
    
    OPTIONS:
     --tui, -t : Launch TUI
     --step, -s : Begin from a specific step
     --help, -h : Show help
     --version, -v : Show version
    
    Disable root checking for development

--- pym/stager/main.py
+++ pym/stager/main.py
@@ -1,2 +1,57 @@
+import argparse
+import os
+
 def main():
-    print("Hello from main")
+    try:
+        #if os.geteuid() != 0:
+        #    raise PermissionError()
+
+        parser = argparse.ArgumentParser(
+                description="Stager is designed to aid users to install Gentoo Linux",
+                epilog="If you find this useful or find a bug please contact to chrisadr@gentoo.org")
+
+        parser.add_argument("-t", "--tui",
+                action="store_true",
+                help="launch Terminal User Interface")
+
+        parser.add_argument("-v", "--version",
+                action="version",
+                version="%(prog)s 0.1.0dev")
+        
+        subparsers = parser.add_subparsers(
+                title="available subcommands",
+                description="Stager implements two ways of installing Gentoo Linux,\
+                from scratch and from an existing system.",
+                help="for specific info use stager <command> --help",
+                metavar="<command>",
+                dest="action")
+
+        begin_parser = subparsers.add_parser("begin",
+                help="begin a new Gentoo Linux installation")
+
+        begin_parser.add_argument("-s", "--step",
+                metavar="N",
+                default=0,
+                choices=[1,2],
+                type=int,
+                help="begin on a specific step of installation")
+
+        generate_parser = subparsers.add_parser("generate",
+                help="generate a stageX tarball from current system")
+
+        args = parser.parse_args()
+
+        if args.action=="begin":
+            import pym.stager.begin as begin
+            begin.init(args)
+        elif args.action=="generate":
+            import pym.stager.generate as generate
+            generate.init(args)
+        else:
+            raise ValueError()
+
+    except ValueError:
+        print("You need to provide an action, see stager --help or -h for more info")
+    except PermissionError:
+        print("You need to be root for using stager")
+        
