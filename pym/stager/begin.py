0b26aaf6e581e208a245661c670b5e182836a6a9
Christopher Díaz Riveros <chrisadr@gentoo.org>
2017-12-19 19:25:17 -0500

    pym/stager/begin.py: Added --step logic
    Check for --step option and begin the tutorial or resumen on the given
    step.

--- pym/stager/begin.py
+++ pym/stager/begin.py
@@ -1,3 +1,14 @@
+import pym.stager.walkthrough as walkthrough 
+
+
 def init(args):
-    print("Now on begin")
-    print(args)
+    """The init function takes args from main program and parses them.
+
+    Possible args:
+    
+    @step: wheter to select a specific installation step
+    """
+    if args.step != 0:
+        walkthrough.resume(args.step,args)
+    else:
+        walkthrough.begin(args)
