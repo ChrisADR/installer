7c67ef172cec51b3c36ce02d51f888924734bd01
Christopher Díaz Riveros <chrisadr@gentoo.org>
2018-01-13 13:53:10 -0500

    step11.py: Added to project
    includes 3 sections
    
    -grub install
    -grub config
    -finish_msg
    
    Signed-off-by: Christopher Díaz Riveros <chrisadr@gentoo.org>

---
+++ pym/stager/step11.py
@@ -0,0 +1,106 @@
+#Copyright (C) Christopher Díaz Riveros <chrisadr@gentoo.org>
+#
+#step11.py is part of Stager.
+#
+#Stager is free software: you can redistribute it and/or modify
+#it under the terms of the GNU General Public License v2 as published by
+#the Free Software Foundation, either version 2 of the License, or
+#(at your option) any later version.
+#
+#Stager is distributed in the hope that it will be useful,
+#but WITHOUT ANY WARRANTY; without even the implied warranty of
+#MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+#GNU General Public License v2 for more details.
+#
+#You should have received a copy of the GNU General Public License v2
+#along with Stager.  If not, see <http://www.gnu.org/licenses/>.
+
+import pym.stager.oshelper as oshelper
+
+welcome_msg="""\
+                       Step 11 - Configuring the bootloader
+================================================================================
+Last step in the list! You are about to finish the Gentoo installation process!!
+Now we need a bootloader, we'll recommend grub, but feel free to install other
+if you want to.
+
+Note: UEFI users, please make sure that you have GRUB_PLATFORMS=\"efi-64\" in
+your /etc/portage/make.conf file before emerging grub, not doing so may cause
+unexpected results.
+
+    echo 'GRUB_PLATFORMS=\"efi-64\"' >> /etc/portage/make.conf
+
+    emerge --ask sys-boot/grub
+
+Then install the bootloader with:
+
+    grub-install /dev/sda
+
+WARNING: UEFI users are highly encouraged to read following section before 
+installing grub:
+https://wiki.gentoo.org/wiki/Handbook:AMD64/Installation/Bootloader#Install
+"""
+
+config_msg="""\
+Now you need to configure grub, this way it'll be able to find your partitions
+and initramfs. Execute:
+
+    grub-mkconfig -o /boot/grub/grub.cfg
+
+If you have no error messages, CONGRATULATIONS! you have now a fully installed
+Gentoo system. But before finishing this tutorial, you need to check a couple
+things:
+
+-Start/enable all the needed services: e.g. if you installed NetworkManager:
+
+    systemctl enable NetworkManager         (SystemD)
+    rc-update add NetworkManager default    (OpenRC)
+-Install your Window Manager or Desktop Environment: e.g. GNOME or Plasma
+    
+    emerge --ask [gnome|plasma|i3]
+-Create a new user:
+
+    useradd -m -G users,wheel,audio -s /bin/bash larry
+    passwd larry
+"""
+
+finish_msg="""\
+Finally, you need to exit the chroot and umount all the linked directories:
+
+    umount -l /mnt/gentoo/dev{/shm,/pts,}
+    umount -R /mnt/gentoo
+
+We highly recommend you to read these sections to be able to work with Gentoo
+and Portage:
+
+https://wiki.gentoo.org/wiki/Handbook:AMD64/Full/Working
+https://wiki.gentoo.org/wiki/Handbook:AMD64/Full/Portage
+
+If you are reading this message this could mean that now you have your own
+Gentoo system, thank your for trying stager and if you find any bug, or error,
+even if you have enhancement ideas, feel free to email me:
+
+    chrisadr@gentoo.org
+
+Thank you :) and enjoy Gentoo!
+"""
+
+def init(args):
+    if args.tui:
+        pass
+    else:
+        prepare_bootloader()
+        prepare_config()
+        prepare_finish()
+
+def prepare_bootloader():
+    global welcome_msg
+    oshelper.show_msg_open_shell(welcome_msg)
+
+def prepare_config():
+    global config_msg
+    oshelper.show_msg_open_shell(config_msg)
+
+def prepare_finish():
+    global finish_msg
+    oshelper.show_msg(finish_msg)
