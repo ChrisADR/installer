feae6b1e6140d2ffa67224a8f02e29e2324cad96
Christopher Díaz Riveros <chrisadr@gentoo.org>
2018-01-14 12:56:51 -0500

    generate.py: Initial implementation
    generate subcommand will create a stageX-date.tar.xz tarball which will
    contain the following files:
    
    	/etc/portage/*
    	/etc/timezone
    	/etc/locale.gen
    	/var/lib/portage/world
    
    Signed-off-by: Christopher Díaz Riveros <chrisadr@gentoo.org>

--- pym/installer/generate.py
+++ pym/installer/generate.py
@@ -14,3 +14,36 @@
 #
 #You should have received a copy of the GNU General Public License v2
 #along with Installer.  If not, see <http://www.gnu.org/licenses/>.
+
+import pym.installer.oshelper as oshelper
+
+welcome_msg="""\
+                            Welcome to generate command
+================================================================================
+I'll prepare a stageX tarball, after extracting a regular stage3 tarball during
+installation, you can extract stageX tarball and you'll have most of the files
+needed to finish installation, some of these files include:
+
+    /etc/portage/* : All the required files and subdirectories.
+    /var/lib/portage/world : List of currently installed programs.
+    /etc/timezone : Current timezone
+    /etc/locale.gen : Current locales
+
+Once you extract stageX tarball you'll still need to:
+
+    Update your repository (emerge-webrsync)
+    Update @world
+    Configure timezone and locales
+    Update your /etc/fstab file
+    Configure your kenrel.
+    Configure your bootloader.
+"""
+
+def init(args):
+    prepare_generate()
+    oshelper.generate_default_stagex()
+
+
+def prepare_generate():
+    global welcome_msg
+    oshelper.show_msg(welcome_msg)
