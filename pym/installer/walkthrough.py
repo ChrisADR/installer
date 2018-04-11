bc55e130feaa543f4ab36bfb2c8a1692f6426d36
Christopher Diaz Riveros <chrisadr@gentoo.org>
2018-04-11 17:23:13 -0500

    walkthrough.py: Refactor imports and init calls
    Move all import statements to the beginning from the file and remove all
    args arguments in init functions
    
    Signed-off-by: Christopher Diaz Riveros <chrisadr@gentoo.org>

--- pym/installer/walkthrough.py
+++ pym/installer/walkthrough.py
@@ -15,52 +15,44 @@
 #You should have received a copy of the GNU General Public License v2
 #along with Installer.  If not, see <http://www.gnu.org/licenses/>.
 
-def begin(args):
+import pym.installer.step1 as step1
+import pym.installer.step2 as step2
+import pym.installer.step3 as step3
+import pym.installer.step4 as step4
+import pym.installer.step5 as step5
+import pym.installer.step6 as step6
+import pym.installer.step7 as step7
+import pym.installer.step8 as step8
+import pym.installer.step9 as step9
+import pym.installer.step10 as step10
+import pym.installer.step11 as step11
+
+
+def begin(index):
     """Begin function starts the interactive tutorial for installing Gentoo Linux.
-    Possible args:
+    Possible arguments:
 
-    @tui: wheter to display information in TUI or CLI 
+    @index: defines in which step begin.
     """
-    import pym.installer.step1 as step1
-    step1.init(args)
-
-
-def resume(step,args):
-    """Resume function resumes the interactive tutorial from a specific step in tutorial.
-    Possible args:
-
-    @tui: wheter to display information in TUI or CLI
-    """
-    if step==1:
-        import pym.installer.step1 as step1
-        step1.init(args)
-    elif step==2:
-        import pym.installer.step2 as step2
-        step2.init(args)
-    elif step==3:
-        import pym.installer.step3 as step3
-        step3.init(args)
-    elif step==4:
-        import pym.installer.step4 as step4
-        step4.init(args)
-    elif step==5:
-        import pym.installer.step5 as step5
-        step5.init(args)
-    elif step==6:
-        import pym.installer.step6 as step6
-        step6.init(args)
-    elif step==7:
-        import pym.installer.step7 as step7
-        step7.init(args)
-    elif step==8:
-        import pym.installer.step8 as step8
-        step8.init(args)
-    elif step==9:
-        import pym.installer.step9 as step9
-        step9.init(args)
-    elif step==10:
-        import pym.installer.step10 as step10
-        step10.init(args)
-    elif step==11:
-        import pym.installer.step11 as step11
-        step11.init(args)
+    if not index or index == 1:
+        step1.init()
+    elif index==2:
+        step2.init()
+    elif index==3:
+        step3.init()
+    elif index==4:
+        step4.init()
+    elif index==5:
+        step5.init()
+    elif index==6:
+        step6.init()
+    elif index==7:
+        step7.init()
+    elif index==8:
+        step8.init()
+    elif index==9:
+        step9.init()
+    elif index==10:
+        step10.init()
+    elif index==11:
+        step11.init()
