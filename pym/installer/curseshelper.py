f1bdf5b01c6c28a2f628a28a670a2729145a4b23
Christopher Diaz Riveros <chrisadr@gentoo.org>
2018-04-11 17:25:20 -0500

    curseshelper.py: Generate windows for TUI
    Curses helper implements the following functions:
    
    - launch_tui
    - generate_tui
    - finish_windows
    - prepare_curses
    - generate_title_window
    - generate_info_window
    - generate_arrow_window
    - print_arrow
    - launch_step
    
    Signed-off-by: Christopher Diaz Riveros <chrisadr@gentoo.org>

--- pym/installer/curseshelper.py
+++ pym/installer/curseshelper.py
@@ -15,3 +15,84 @@
 #You should have received a copy of the GNU General Public License v2
 #along with Installer.  If not, see <http://www.gnu.org/licenses/>.
 
+import curses
+from curses import wrapper
+
+import pym.installer.walkthrough as walkthrough
+
+def launch_tui():
+    stdscr = curses.initscr()
+    wrapper(generate_tui)
+
+def generate_tui(stdscr):
+    stdscr.clear()
+    prepare_curses(stdscr)
+    stdscr.refresh()
+    generate_title_window(stdscr)
+    generate_info_window(stdscr)
+    generate_arrow_window(stdscr)
+
+
+def finish_windows():
+    curses.endwin()
+
+def prepare_curses(stdscr):
+    curses.noecho()
+    curses.cbreak()
+    curses.curs_set(0)
+
+def generate_title_window(stdscr):
+    title_win = curses.newwin(4,curses.COLS-2,0,10)
+    title_win.addstr(1,12,"Welcome to installer TUI.")
+    title_win.addstr(3,2,"Use the arrow to select the step and press ENTER")
+    title_win.refresh()
+
+def generate_info_window(stdscr):
+    info_win = curses.newwin(13,50,5,12)
+    info_win.box()
+    info_win.addstr(1,2,"Step 1: Welcome to Gentoo installer!")
+    info_win.addstr(2,2,"Step 2: Preparing the disks")
+    info_win.addstr(3,2,"Step 3: Installing Stage3")
+    info_win.addstr(4,2,"Step 4: Configuring compile options")
+    info_win.addstr(5,2,"Step 5: Installing the Gentoo base system")
+    info_win.addstr(6,2,"Step 6: Entering the chroot")
+    info_win.addstr(7,2,"Step 7: Configuring Portage")
+    info_win.addstr(8,2,"Step 8: Configuring the system")
+    info_win.addstr(9,2,"Step 9: Configuring the kernel")
+    info_win.addstr(10,2,"Step 10: Configuring the system")
+    info_win.addstr(11,2,"Step 11: Configuring the bootloader")
+    info_win.refresh()
+
+def generate_arrow_window(stdscr):
+    select_win = curses.newwin(13,5,5,7)
+    select_win.box()
+    select_win.keypad(True)
+    index = 1
+    print_arrow(select_win,index)
+    while True:
+        key = select_win.getch()
+        if key == curses.KEY_UP:
+            if (index-1) <= 0:
+                index = 11
+            else:
+                index-=1
+            print_arrow(select_win, index)
+        elif key == curses.KEY_DOWN:
+            if (index+1) >= 12:
+                index = 1
+            else:
+                index+=1
+            print_arrow(select_win,index)
+        elif key in [curses.KEY_ENTER, ord('\n')]:
+            break
+    launch_step(index)
+
+def print_arrow(select_win,index):
+    for value in range(1,12):
+        select_win.addstr(value,1,"   ")
+    select_win.addstr(index,1," =>")
+    select_win.refresh()
+
+def launch_step(index):
+    finish_windows()
+    walkthrough.begin(index)
